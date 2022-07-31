2c9b0e6ea5b54cb0c0e5fd89ec75de8947c9588b
RitvarsZ <ritvarszvejnieks@protonmail.com>
2022-07-31 20:18:19 +0300

    Add user create artisan command

---
+++ app/Console/Commands/CreateUser.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\User;
+
+class CreateUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'user:create';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Register a new user';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $name = $this->ask('What is your name?');
+        $email = $this->ask('What is your email?');
+        $password = $this->secret('What is your password?');
+        $confirm = $this->secret('Confirm your password');
+
+        if ($password !== $confirm) {
+            $this->error('Passwords do not match.');
+            return 1;
+        }
+
+        $user = new User;
+        $user->name = $name;
+        $user->email = $email;
+        $user->password = bcrypt($password);
+        $user->save();
+
+        $this->info("User {$user->name} created successfully.");
+    }
+}
